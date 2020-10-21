ff542b7b5eeb501e73ac690f72e96b2dc4ac6e0b
Luciano <lucianomm98@gmail.com>
2020-10-21 14:10:48 -0300

    Esqueleto da função Roteador Virtual

--- RoteadorVirtual.py
+++ RoteadorVirtual.py
@@ -1,3 +1,27 @@
+import socket
+import os
+import json
+import threading
+
+cmds = (
+    "add <ip> <weight>",
+    "del <ip>"
+)
+
+def addIP(ip,weight):
+    pass
+
+def deleteIP(ip):
+    pass
+
+def recvCmd(argv1,argv2,argv3=None):
+    if argv1 == 'add':
+        addIP(argv2,argv3)
+    elif argv1 == 'del':
+        deleteIP(argv2)
+
+
 if __name__ == "__main__":
-    print('Hello world')
+    print("Comandos de interface disponíveis:")
+    print('\n'.join(cmds))
     pass
