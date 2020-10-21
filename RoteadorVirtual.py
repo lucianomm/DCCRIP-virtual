1ce11e516eaf7bd1e294b9d454e7084d9ae8863b
Luciano <lucianomm98@gmail.com>
2020-10-21 14:23:04 -0300

    Criando documentação de cada função no arquivo

--- RoteadorVirtual.py
+++ RoteadorVirtual.py
@@ -9,17 +9,37 @@ cmds = (
 )
 
 def addIP(ip,weight):
+    '''
+    Adiciona um IP com o peso (custo de envio) para o banco de dados do Roteador
+    '''
     pass
 
 def deleteIP(ip):
+    '''
+    Deleta um IP do banco de dados do roteador
+    '''
     pass
 
 def recvCmd(argv1,argv2,argv3=None):
+    '''
+    Trata os comandos recebidos pelo usuário
+    '''
     if argv1 == 'add':
         addIP(argv2,argv3)
     elif argv1 == 'del':
         deleteIP(argv2)
 
+def messageJson(type,source,destination,payload = None):
+    '''
+    Recebe os parâmetros de possíveis mensagens e transforma em pacotes Json para envio padronizado
+    '''
+    pass
+
+def updateRoutes():
+    '''
+    Atualiza as rotas conhecidas pelo roteador
+    '''
+    pass
 
 if __name__ == "__main__":
     print("Comandos de interface disponíveis:")
